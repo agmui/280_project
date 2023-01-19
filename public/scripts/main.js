c86c8a1f05bf984c5b726193054f557386350c2b
rhit-vorheeks <vorheeks@rose-hulman.edu>
2023-01-19 14:58:03 -0500

    clean file

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,25 +1,11 @@
-/**
- * @fileoverview
- * Provides the JavaScript interactions for all pages.
- *
- * @author 
- * PUT_YOUR_NAME_HERE
- */
 
-/** namespace. */
 var rhit = rhit || {};
 
-/** globals */
-rhit.variableName = "";
 
-/** function and class syntax examples */
-rhit.functionName = function () {
-	/** function body */
-};
 
-rhit.ClassName = class {
+rhit.item = class {
 	constructor() {
-
+		
 	}
 
 	methodName() {
@@ -27,8 +13,6 @@ rhit.ClassName = class {
 	}
 }
 
-/* Main */
-/** function and class syntax examples */
 rhit.main = function () {
 	console.log("Ready");
 };
