86c55b3a0c94b4c5603745598fdf83bff5a95289
anthony_nobara <anthonynmui@gmail.com>
2023-01-19 15:09:26 -0500

    added method stubs to user and item

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,7 +1,30 @@
 
 var rhit = rhit || {};
 
+function addItem(itemName) {
+	
+}
+function deleteItem(itemName) {
+	
+}
+function queryItem(itemSubString){
 
+}
+function checkoutItem(itemName, userName) {
+
+}
+function returnItem(itemName, userName) {
+
+}
+function getUser(userName) {
+
+}
+
+rhit.Controller = class {
+	constructor () {
+
+	}
+}
 
 rhit.item = class {
 	constructor() {
@@ -9,6 +32,15 @@ rhit.item = class {
 	}
 
 	methodName() {
+
+	}
+}
+rhit.User = class {
+	constructor(username) {
+		this.userName = username
+	}
+
+	editUser () {
 
 	}
 }
