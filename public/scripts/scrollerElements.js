2bf725994d7b1d7e265345e0b007cd932a1fed84
rhit-vorheeks <vorheeks@rose-hulman.edu>
2023-02-07 14:36:23 -0500

    arrow edit

--- public/scripts/scrollerElements.js
+++ public/scripts/scrollerElements.js
@@ -19,8 +19,18 @@ function updateDownArrow() {
     });
 }
 
+function setupDownArrow() {
+    var scrollerElements = document.querySelectorAll(".down-arrow");
+    scrollerElements.forEach(element => {
+        if (window.scrollY > 80 || window.innerHeight > 800) {
+            element.style.display = "none";
+          }
+    });
+}
+
 window.addEventListener("scroll", function () {
     update()
     updateDownArrow()
 });
+setupDownArrow()
 update()
